9d7b75292264c717f1178a669c52a339eef5ab4c
Metal "Pizza" Cat <36876492+MetalPizzaCat@users.noreply.github.com>
2024-06-21 23:58:14 +1000

    fix node not being found

--- editor/graph_edit.gd
+++ editor/graph_edit.gd
@@ -7,12 +7,18 @@ func _ready():
 	if node_popup != null:
 		node_popup.node_requsted.connect(create_node)
 
+
 func _on_connection_request(
 	from_node: StringName, from_port: int, to_node: StringName, to_port: int
 ):
-	var start = find_child(from_node)
-	var end = find_child(to_node)
-
+	var start = find_child(from_node, false, false)
+	var end = find_child(to_node, false, false)
+	if start == null:
+		printerr("Unable to find start point for connection")
+		return
+	if end == null:
+		printerr("Unable to find end point for connection")
+		return
 	if end.can_connect_on_port(to_port):
 		connect_node(from_node, from_port, to_node, to_port)
 		end.connect_on_port(to_port, start)
@@ -24,8 +30,9 @@ func _on_popup_request(click_position: Vector2):
 	node_popup.position = click_position
 	node_popup.show()
 
-func create_node(data : NodeCreationData):
-	var node : BaseNode
+
+func create_node(data: NodeCreationData):
+	var node: BaseNode
 	if data.use_prefab and data.prefab_scene != null:
 		node = data.prefab_scene.instantiate() as BaseNode
 	else:
